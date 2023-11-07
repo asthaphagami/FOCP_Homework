e0ad6c1630f2c6bdb89b62f57be8a2caaca660f5
asthaphagami <135811099+asthaphagami@users.noreply.github.com>
2023-11-07 20:37:41 +0545

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+print("Hello Astha!")
+
+c = 38.4
+f = (c*(9/5))+32
+print(f)
+
+run = 48426
+batted_times = 1814
+not_out = 162
+innings = batted_times - not_out
+batting_avg = run/innings
+print(batting_avg)
+
+groups_size = 24
+class_size = 113
+full_groups = class_size // groups_size 
+left_overs = class_size % groups_size
+print("No. of full groups:",full_groups)
+print("Left over students:",left_overs)
